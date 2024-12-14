0652109cea94a5aca2391de096b7b9298c21c485
kot149 <131101078+kot149@users.noreply.github.com>
2024-12-14 13:22:11 +0900

    Initialize next-app

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/join-ixv',
+}
+
+module.exports = nextConfig
